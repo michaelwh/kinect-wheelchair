12400875d0a736e50d91a59488d6e205ba8f60a9
Michael Hodgson <michaelwhodgson@gmail.com>
2012-05-21 19:58:28 +0100

    added sample code for nice RGB cloud viewer

--- src/kinect_wheelchair.cpp
+++ src/kinect_wheelchair.cpp
@@ -1,90 +1,244 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2010, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $Id: openni_viewer.cpp 4360 2012-02-10 01:01:11Z rusu $
+ *
+ */
+
+#include <boost/thread/thread.hpp>
+#define MEASURE_FUNCTION_TIME
+#include <pcl/common/time.h> //fps calculations
 #include <pcl/io/openni_grabber.h>
-#include <pcl/visualization/cloud_viewer.h>
-#include <pcl/filters/passthrough.h>
-//#ifdef _WIN32
-// # define sleep(x) Sleep((x)*1000)
-// #endif
+#include <pcl/visualization/point_cloud_handlers.h>
+#include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/visualization/image_viewer.h>
+#include <pcl/console/print.h>
+#include <pcl/console/parse.h>
+#include <pcl/console/time.h>
 
+#define SHOW_FPS 1
+#if SHOW_FPS
+#define FPS_CALC(_WHAT_) \
+do \
+{ \
+    static unsigned count = 0;\
+    static double last = pcl::getTime ();\
+    double now = pcl::getTime (); \
+    ++count; \
+    if (now - last >= 1.0) \
+    { \
+      std::cout << "Average framerate("<< _WHAT_ << "): " << double(count)/double(now - last) << " Hz" <<  std::endl; \
+      count = 0; \
+      last = now; \
+    } \
+}while(false)
+#else
+#define FPS_CALC(_WHAT_) \
+do \
+{ \
+}while(false)
+#endif
 
-// the base franework for this from http://www.pointclouds.org/documentation/tutorials/openni_grabber.php
-// and http://pointclouds.org/documentation/tutorials/cloud_viewer.php
-class SimpleOpenNIViewer
+boost::mutex cld_mutex, img_mutex;
+pcl::PointCloud<pcl::PointXYZRGBA>::ConstPtr g_cloud;
+boost::shared_ptr<openni_wrapper::Image> g_image;
+
+void
+printHelp (int argc, char **argv)
 {
-public:
-
-	SimpleOpenNIViewer() : viewer("PCL OpenNI Viewer") {}
-
-	void viewerOneOff(pcl::visualization::PCLVisualizer& pclViewer)	
-	{
-		pcl::ModelCoefficients plane_coeff;
-		plane_coeff.values.resize(4);
-		plane_coeff.values[0] = 0.0; // x
-		plane_coeff.values[1] = -1.0; // y
-		plane_coeff.values[2] = 0.0; // z
-		plane_coeff.values[3] = 0.0; // d
-		pclViewer.
-		pclViewer.addPlane(plane_coeff, "ground_test_plane", 0);
-	}
-
-	void viewerIteration(pcl::visualization::PCLVisualizer& pclViewer)
-	{
-
-	}
-
-	void cloud_cb_(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr &cloud)
-	{
-
-		if (!viewer.wasStopped()) {
-			//pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZ>);
-
-			//pcl::PassThrough<pcl::PointXYZ> pass;
-			//pass.setInputCloud(cloud);
-			//pass.setFilterFieldName("z");
-			//pass.setFilterLimits(0.0, 1.0);
-			//pass.filter(*cloud_filtered);
-			viewer.showCloud(cloud);			//viewer.showCloud(cloud_filtered);
-
-		}
-	}
-
-	void run()
-	{
-		pcl::Grabber* grabber = new pcl::OpenNIGrabber();
-
-		boost::function<void (const pcl::PointCloud<pcl::PointXYZ>::ConstPtr&)> f =
-			boost::bind(&SimpleOpenNIViewer::cloud_cb_, this, _1);
-
-		boost::function<void (pcl::visualization::PCLVisualizer&)> fVOnOff =
-			boost::bind(&SimpleOpenNIViewer::viewerOneOff, this, _1);
-
-		boost::function<void (pcl::visualization::PCLVisualizer&)> fVIteration =
-			boost::bind(&SimpleOpenNIViewer::viewerIteration, this, _1);
-	
-		
-		viewer.runOnVisualizationThreadOnce(fVOnOff);
-		viewer.runOnVisualizationThread(fVIteration);
-
-		grabber->registerCallback(f);
-		
-		grabber->start();
-
-		
-
-
-		while (!viewer.wasStopped())
-		{
-			boost::this_thread::sleep(boost::posix_time::seconds(1));
-		}
-
-		grabber->stop();
-	}
-
-	pcl::visualization::CloudViewer viewer;
-};
-
-int main()
-{
-	SimpleOpenNIViewer v;
-	v.run();
-	return 0;
+  using pcl::console::print_error;
+  using pcl::console::print_info;
+  print_error ("Syntax is: %s <options>\n", argv[0]);
+  print_info ("  where options are:\n");
+  print_info ("                     -dev device_id           = device to be used\n");
+  print_info ("                                                maybe \"#n\", with n being the number of the device in device list.\n");
+  print_info ("                                                maybe \"bus@addr\", with bus and addr being the usb bus and address where device is connected.\n");
+  print_info ("                                                maybe \"serial\", with serial being the serial number of the device.\n");
+  print_info ("\n");
 }
+
+// Create the PCLVisualizer object
+boost::shared_ptr<pcl::visualization::PCLVisualizer> cld;
+#if !((VTK_MAJOR_VERSION == 5)&&(VTK_MINOR_VERSION <= 4))
+boost::shared_ptr<pcl::visualization::ImageViewer> img;
+#endif
+
+struct EventHelper
+{
+  void 
+  cloud_cb (const pcl::PointCloud<pcl::PointXYZRGBA>::ConstPtr & cloud)
+  {
+    FPS_CALC ("callback");
+    cld_mutex.lock ();
+    g_cloud = cloud;
+    cld_mutex.unlock ();
+  }
+
+#if !((VTK_MAJOR_VERSION == 5)&&(VTK_MINOR_VERSION <= 4))
+  void
+  image_callback (const boost::shared_ptr<openni_wrapper::Image>& image)
+  {
+    FPS_CALC ("image callback");
+    img_mutex.lock ();
+    g_image = image;
+    img_mutex.unlock ();
+  }
+#endif  
+};
+// Simple callbacks.
+void 
+keyboard_callback (const pcl::visualization::KeyboardEvent& event, void* cookie)
+{
+  std::string* message = (std::string*)cookie;
+  cout << (*message) << " :: ";
+  if (event.getKeyCode())
+    cout << "the key \'" << event.getKeyCode() << "\' (" << (int)event.getKeyCode() << ") was";
+  else
+    cout << "the special key \'" << event.getKeySym() << "\' was";
+  if (event.keyDown())
+    cout << " pressed" << endl;
+  else
+    cout << " released" << endl;
+}
+
+void 
+mouse_callback (const pcl::visualization::MouseEvent& mouse_event, void* cookie)
+{
+  std::string* message = (std::string*) cookie;
+  if (mouse_event.getType() == pcl::visualization::MouseEvent::MouseButtonPress && mouse_event.getButton() == pcl::visualization::MouseEvent::LeftButton)
+  {
+    cout << (*message) << " :: " << mouse_event.getX () << " , " << mouse_event.getY () << endl;
+  }
+}
+
+/* ---[ */
+int
+main (int argc, char** argv)
+{
+  if (argc > 1)
+  {
+    for (int i = 1; i < argc; i++)
+    {
+      if (std::string (argv[i]) == "-h")
+      {
+        printHelp (argc, argv);
+        return (-1);
+      }
+    }
+  }
+
+  EventHelper event_helper;
+  std::string device_id = "";
+  pcl::console::parse_argument (argc, argv, "-dev", device_id);
+
+  pcl::Grabber* viewer = new pcl::OpenNIGrabber (device_id);
+
+  cld.reset (new pcl::visualization::PCLVisualizer (argc, argv, "OpenNI Viewer"));
+
+  std::string mouseMsg3D ("Mouse coordinates in PCL Visualizer");
+  std::string keyMsg3D ("Key event for PCL Visualizer");
+  cld->registerMouseCallback (&mouse_callback, (void*)(&mouseMsg3D));    
+  cld->registerKeyboardCallback(&keyboard_callback, (void*)(&keyMsg3D));
+  boost::function<void(const pcl::PointCloud<pcl::PointXYZRGBA>::ConstPtr&) > f = boost::bind (&EventHelper::cloud_cb, &event_helper, _1);
+  boost::signals2::connection c1 = viewer->registerCallback (f);
+
+#if !((VTK_MAJOR_VERSION == 5)&&(VTK_MINOR_VERSION <= 4))
+  img.reset (new pcl::visualization::ImageViewer ("OpenNI Viewer"));
+  // Register callbacks
+  std::string keyMsg2D ("Key event for image viewer");
+  std::string mouseMsg2D ("Mouse coordinates in image viewer");
+  img->registerMouseCallback (&mouse_callback, (void*)(&mouseMsg2D));
+  img->registerKeyboardCallback(&keyboard_callback, (void*)(&keyMsg2D));
+  boost::function<void (const boost::shared_ptr<openni_wrapper::Image>&) > image_cb = boost::bind (&EventHelper::image_callback, &event_helper, _1);
+  boost::signals2::connection image_connection = viewer->registerCallback (image_cb);
+  unsigned char* rgb_data = 0;
+  unsigned rgb_data_size = 0;
+#endif 
+  
+  viewer->start();
+  bool cld_init = false;
+  // Loop
+  while (!cld->wasStopped ())
+  {
+    // Render and process events in the two interactors
+    cld->spinOnce ();
+#if !((VTK_MAJOR_VERSION == 5)&&(VTK_MINOR_VERSION <= 4))
+    img->spinOnce ();
+#endif
+    FPS_CALC ("drawing");
+
+    // Add the cloud
+    if (g_cloud && cld_mutex.try_lock ())
+    {
+      if (!cld_init)
+      {
+        cld->getRenderWindow ()->SetSize (g_cloud->width, g_cloud->height);
+        cld->getRenderWindow ()->SetPosition (g_cloud->width, 0);
+        cld_init = !cld_init;
+      }
+
+      pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGBA> handler (g_cloud);
+      if (!cld->updatePointCloud (g_cloud, handler, "OpenNICloud"))
+      {
+        cld->addPointCloud (g_cloud, handler, "OpenNICloud");
+        cld->resetCameraViewpoint ("OpenNICloud");
+      }
+      cld_mutex.unlock ();
+    }
+    
+#if !((VTK_MAJOR_VERSION == 5)&&(VTK_MINOR_VERSION <= 4))
+    // Add the image
+    if (g_image && img_mutex.try_lock ())
+    {
+      if (g_image->getEncoding() == openni_wrapper::Image::RGB)
+        img->showRGBImage (g_image->getMetaData ().Data (), 
+                           g_image->getWidth (), g_image->getHeight ());
+      else
+      {
+        if (rgb_data_size < g_image->getWidth () * g_image->getHeight ())
+        {
+          rgb_data_size = g_image->getWidth () * g_image->getHeight ();
+          rgb_data = new unsigned char [rgb_data_size * 3];
+        }
+        g_image->fillRGB (g_image->getWidth (), g_image->getHeight (), rgb_data);
+        img->showRGBImage (rgb_data, g_image->getWidth (), g_image->getHeight ());
+      }
+      img_mutex.unlock ();
+    }
+#endif
+    boost::this_thread::sleep (boost::posix_time::microseconds (100));
+  }
+
+  viewer->stop ();
+}
+/* ]--- */
